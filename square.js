083c5fd9d4330d0c75740a5c35d88f10fb292973
Claudiu Andrei <claudiu.andrei@docusign.com>
2015-08-01 21:00:21 -0700

    Fix issues with data

--- square.js
+++ square.js
@@ -1,13 +1,30 @@
 var Promise = require('bluebird');
-var Square = Promise.promisifyAll(require('square-connect'));
-var Postmates = Promise.promisifyAll(require('postmates'));
+var request = Promise.promisifyAll(require("request"));
+var Postmates = require('postmates');
 var Firebase = require('firebase');
 var _ = require('lodash');
+var phoneFormatter = require('phone-formatter');
 var config = require('./config');
 
 // Setup the square client
-var square = new Square(config.square.apiKey);
-var postmates = new Postmates(config.postmates.customerId, config.postmates.apiKey)
+var postmates = Promise.promisifyAll(new Postmates(config.postmates.customerId, config.postmates.apiKey));
+
+// Stringify shipping address
+var stringifyAddress = function (address) {
+  return _.chain(address)
+    .pick(_.identity)
+    .values()
+    .join(', ')
+    .value();
+}
+
+var formatNumber = function (number, format) {
+  format = format || '(NNN) NNN-NNNN';
+  if (_.isObject(number)) {
+    number = _.values(number).join();
+  }
+  return phoneFormatter.format(number, format);
+}
 
 // Entry point
 module.exports = function (req, res) {
@@ -15,55 +32,64 @@ module.exports = function (req, res) {
   var data = {}
   var event = req.body;
 
+  var square = function(url) {
+    return request.getAsync('https://connect.squareup.com/v1/' + url, {
+      'auth': {
+        'bearer': config.square.apiKey
+      },
+      json: true
+    });
+  }
+
   // Get orders, payment, merchant
-  Promise.all({
-    orders: square.api('me/orders'),
-    payment: square.api('me/payments/' + event.entity_id),
-    merchant: square.api('me')
-  }).then(function (res) {
+  Promise.props({
+    orders: square('me/orders'),
+    payment: square('me/payments/' + event.entity_id),
+    merchant: square('me')
+  }).then(function (response) {
 
       // Load the orders
-      var orders = res.orders.body;
+      var orders = _.first(response.orders).body;
 
       // Load the order
       data.order = _.find(orders, { payment_id: event.entity_id })
-      data.merchant = res.merchant.body;
-      data.payment = res.payment.body;
+      data.merchant = _.first(response.merchant).body;
+      data.payment = _.first(response.payment).body;
 
       // Quote
-      return postmates.quote({
-        pickup_address: data.merchant.shipping_address,
-        dropoff_address: data.order.shipping_address
+      return postmates.quoteAsync({
+        pickup_address: stringifyAddress(data.merchant.shipping_address),
+        dropoff_address: stringifyAddress(data.order.shipping_address)
       });
 
-    }).then(function (res) {
+    }).then(function (response) {
 
       // Keep data reference
-      data.quote = res.body;
+      data.quote = response.body;
 
       // We overpay
-      if (data.order.total_shipping_money > data.quote.fee) {
+      /*if (data.order.total_shipping_money > data.quote.fee) {
         throw new Error('Overpay');
-      }
+      }*/
 
       // Return the delivery
-      return postmates.new({
+      return postmates.newAsync({
         manifest: _.pluck(data.payment.itemizations, 'name').join(', '),
         pickup_name: data.merchant.business_name,
-        pickup_address: data.merchant.shipping_address,
-        pickup_phone_number: data.merchant.business_phone,
+        pickup_address: stringifyAddress(data.merchant.shipping_address),
+        pickup_phone_number: formatNumber(data.merchant.business_phone),
         pickup_business_name: data.merchant.business_name,
         dropoff_name: data.order.recipient_name,
-        dropoff_address: data.order.shipping_address,
-        dropoff_phone_number: data.order.recipient_phone_number,
+        dropoff_address: stringifyAddress(data.order.shipping_address),
+        dropoff_phone_number: formatNumber(data.order.recipient_phone_number),
         dropoff_notes: data.order.buyer_note,
         quote_id: data.quote.id
       });
 
-    }).then(function (res) {
+    }).then(function (response) {
 
       // Delivery info
-      data.delivery = res.body;
+      data.delivery = response.body;
 
       var url = config.firebase.url + '/orders/' + data.delivery.id;
       var firebaseRef = new Firebase(url);
