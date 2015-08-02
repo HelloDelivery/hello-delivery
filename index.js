981d8eabfeec73624d012eb52e2d1fcb354b014e
Claudiu Andrei <claudiu.andrei@docusign.com>
2015-08-01 18:13:53 -0700

    post

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ var square = require('./square')
 var postmates = require('./postmates')
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
 
 // Listener to postmates
-app.use('/postmates', postmates)
-app.use('/square', square)
+app.post('/postmates', postmates)
+app.post('/square', square)
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
