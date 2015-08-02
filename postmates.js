40af90411ce6353a1aaf6b4e6c2e85e31722bea2
Claudiu Andrei <claudiu.andrei@docusign.com>
2015-08-01 21:19:25 -0700

    Push a history

--- postmates.js
+++ postmates.js
@@ -7,9 +7,9 @@ module.exports = function (req, res) {
   var delivery = req.body.data;
 
   // Set the info to firebase
-  var url = config.firebase.url + '/orders/' + delivery.id + '/delivery';
+  var url = config.firebase.url + '/orders/' + delivery.id + '/delivery_history';
   var firebaseRef = new Firebase(url);
-  firebaseRef.set(delivery);
+  firebaseRef.push(delivery);
 
   // Send the response
   res.send();
